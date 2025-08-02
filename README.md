ca19a29901970a6d09b8b79959b040bfed103e00
Kamran <Yousef.shaikhan@gmail.com>
2025-08-02 22:38:49 +0100

    Added_simple _calculation
    Add a simple calculation to return different messages for 0 jobs, 1-5 jobs, and more than 5 jobs. And also use the Math.floor() method.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # React.js-Skills2.4
+
+Tasks:
+
+1.Complete the JSX to display the company name using curly braces {}.
+
+2.Implement the getJobMessage function:
+Use an if-else statement or a ternary operator to return the appropriate message based on jobCount.
+Use a template literal for the message when there are jobs available.
+
+3.Use JSX expression to call and display the result of getJobMessage() in the paragraph tag.
+
+4.Test your component by changing the jobCount value and observing how the displayed message changes.
+
+///////////////////////
+Bonus Challenges:
+
+1.Add a simple calculation in your JSX. For example, display the expected number of jobs next week by multiplying the current jobCount by 1.5.
+
+2.Create a more complex condition in getJobMessage. For example, return different messages for 0 jobs, 1-5 jobs, and more than 5 jobs.
--- expression/src/Component/JobBoard.js
+++ expression/src/Component/JobBoard.js
@@ -1,16 +1,27 @@
 const JobBoard = () => {
-  const jobCounts = 10;
+  const jobCounts = 4;
   const companyName = "Google";
 
-  const getJobMessage = () => {
-    return jobCounts === 0
-      ? `Today ${companyName} doesn't have any job openings.` // ← if true
-      : `Today ${companyName} has ${jobCounts} job(s) available.`; // ← if false
+  const getJobMessage = () => { //// we Use ternary for simple two-option choices
+    // return jobCounts === 0
+    //   ? `Today ${companyName} doesn't have any job openings.` // ← if true
+    //   : `Today ${companyName} has ${jobCounts} job(s) available.`; // ← if false
+    if (jobCounts === 0) {
+      return `${companyName} doen't have any job for Today.`;
+    } else if(jobCounts <=5) {
+      return `${companyName} has a few job offer(${jobCounts} jobs).`;
+    } else { 
+      return `${companyName} has many job opening (${jobCounts} jobs).`;
+    }
+      
   };
   return (
     <div>
       <h1>Welcome to {companyName} Job Board </h1>
       <p>{getJobMessage()}</p>
+      
+      {/* simple calculation */}
+      <p>Expected jobs next week: {Math.floor(jobCounts * 1.5)}</p>
     </div>
   );
 };
